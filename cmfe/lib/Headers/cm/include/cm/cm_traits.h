ba0e070a7b01cc7c4aff2849a36bd28383a5dc99
Parshintsev Anatoly <anatoly.parshintsev@intel.com>
2021-03-31 14:16:38 +0300

    added implementation of std::conditional template (#105)

--- cmfe/lib/Headers/cm/include/cm/cm_traits.h
+++ cmfe/lib/Headers/cm/include/cm/cm_traits.h
@@ -32,6 +32,12 @@ static_assert(0, "CM:w:cm_traits.h should not be included explicitly - only "
 
 namespace std {
 
+template<bool B, typename T, typename F>
+struct conditional { using type = T; };
+
+template<typename T, typename F>
+struct conditional<false, T, F> { using type = F; };
+
 template <bool, typename T = void> struct enable_if {};
 template <typename T> struct enable_if<true, T> {
   typedef T type;
