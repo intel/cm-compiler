cc0f63bc2dd75bfa239fda835b9ae3dffc4a1133
Aleksander Us <aleksander.us@intel.com>
2021-05-27 15:36:24 +0000

    Remove unknown platforms in test

--- test/CMFE/offline_compiler/ll_tgt.cpp
+++ test/CMFE/offline_compiler/ll_tgt.cpp
@@ -1,13 +1,7 @@
-// RUN: %cmc -mcpu=PVC -emit-llvm -S -o %t.text.ll -- %s
-// RUN: FileCheck -input-file=%t.text.ll --check-prefix=CHECK-PVC %s
 // RUN: %cmc -mcpu=SKL -emit-llvm -S -o %t.text.ll -- %s
 // RUN: FileCheck -input-file=%t.text.ll --check-prefix=CHECK-SKL %s
-// RUN: %cmc -mcpu=TGL -emit-llvm -S -o %t.text.ll -- %s
-// RUN: FileCheck -input-file=%t.text.ll --check-prefix=CHECK-TGL %s
-// CHECK-PVC: "target-cpu"="PVC"
-// CHECK-SKL: "target-cpu"="SKL"
-// CHECK-TGL: "target-cpu"="TGL"
 
+// CHECK-SKL: "target-cpu"="SKL"
 
 #include <cm/cm.h>
 
