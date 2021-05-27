e954472f2143d8f916b4ef2db5e61412e13986cc
Aleksander Us <aleksander.us@intel.com>
2021-05-27 10:22:52 +0000

    Rewrite 2_2_2_a test to check ast

--- test/CMFE/cmlangspec/2_2_2_a.cpp
+++
@@ -1,8 +0,0 @@
-#include <cm/cm.h>
-
-_GENX_ void f() {
-  matrix<short, 4, 8> m;
-  vector_ref<short, 8> r(m.row(2)); // row() returns the reference to a row
-}
-
-// RUN: %cmc -emit-llvm -- %s 2>&1 | FileCheck -allow-empty --implicit-check-not error %s
