6f2a74ff69e735c263c5f8e4894d0f6339362df6
Mikhail Bargatin <mikhail.bargatin@intel.com>
2021-07-05 14:01:54 +0000

    Add list initialization for CM vectors

---
+++ test/CMFE/vec_init/vec_init_codegen_a.cpp
@@ -0,0 +1,10 @@
+// RUN: %cmc -g0 -march=SKL -emit-llvm -S -o %t.ll -- %s
+// RUN: FileCheck %s --input-file %t.ll
+
+#include <cm/cm.h>
+
+_GENX_ void test_func() {
+  vector<unsigned short, 8> vec = {1, 2, 3, 5, 7, 11, 13, 17};
+// CHECK: [[VECTOR:%[^ ]+]] = alloca <8 x i16>
+// CHECK-NEXT: store <8 x i16> <i16 1, i16 2, i16 3, i16 5, i16 7, i16 11, i16 13, i16 17>, <8 x i16>* [[VECTOR]]
+}
---
+++ test/CMFE/vec_init/vec_init_diag.cpp
@@ -0,0 +1,18 @@
+// RUN: not %cmc -emit-llvm -- %s 2>&1 | FileCheck %s
+
+#include <cm/cm.h>
+
+_GENX_ void test_func() {
+  vector<unsigned short, 4> v1 = {};                    // OK
+  vector<unsigned short, 4> v2 = {1, 2};                // OK
+  vector<unsigned short, 4> v3 = {1, 2, 3, 5};          // OK
+  vector<unsigned short, 4> v4 = {1, 2, 3, 5, 7, 11};   // error: excess elements
+  vector<unsigned short, 4> v5 = {{1}};                 // warning: braces around scalar
+  vector<unsigned short, 4> v6 = {{1}, 2, 3, 5, 7, 11}; // warning: braces around scalar; error: excess elements
+  vector<unsigned short, 4> v7 = {(void*) 1, 2, 3, 4};   // error: initializing element with void*
+// CHECK: vec_init_diag.cpp:9:47: error: excess elements in CM vector initializer
+// CHECK: vec_init_diag.cpp:10:35: warning: braces around scalar initializer
+// CHECK: vec_init_diag.cpp:11:35: warning: braces around scalar initializer
+// CHECK: vec_init_diag.cpp:11:49: error: excess elements in CM vector initializer
+// CHECK: vec_init_diag.cpp:12:35: error: cannot initialize a CM vector element of type 'unsigned short' with an rvalue of type 'void *'
+}
