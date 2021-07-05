e28b040bef0338cb656a624208798ba608798ce3
Mikhail Bargatin <mikhail.bargatin@intel.com>
2021-07-05 14:01:54 +0000

    Add list initialization for CM vectors

---
+++ test/CMFE/vec_init/vec_init_ast_b.cpp
@@ -0,0 +1,24 @@
+// RUN: %cmc %dump-ast-for test_func -- %s 2>&1 | FileCheck %s
+
+#include <cm/cm.h>
+
+_GENX_ void test_func() {
+  vector<unsigned short, 8> vec = {1, 2, 3, 5, 7, 11};
+// CHECK:       CompoundStmt 0x{{[^ ]+}} <col:{{[0-9]+}}, line:{{[0-9]+}}:{{[0-9]+}}>
+// CHECK-NEXT:    DeclStmt 0x{{[^ ]+}} <line:{{[0-9]+}}:{{[0-9]+}}, col:{{[0-9]+}}>
+// CHECK-NEXT:      VarDecl 0x{{[^ ]+}} <col:{{[0-9]+}}, col:{{[0-9]+}}> col:{{[0-9]+}} vec 'vector<unsigned short,8>' cinit
+// CHECK-NEXT:        InitListExpr 0x{{[^ ]+}} <col:{{[0-9]+}}, col:{{[0-9]+}}> 'vector<unsigned short,8>'
+// CHECK-NEXT:          ImplicitCastExpr 0x{{[^ ]+}} <col:{{[0-9]+}}> 'unsigned short' <IntegralCast>
+// CHECK-NEXT:            IntegerLiteral 0x{{[^ ]+}} <col:{{[0-9]+}}> 'int' 1
+// CHECK-NEXT:          ImplicitCastExpr 0x{{[^ ]+}} <col:{{[0-9]+}}> 'unsigned short' <IntegralCast>
+// CHECK-NEXT:            IntegerLiteral 0x{{[^ ]+}} <col:{{[0-9]+}}> 'int' 2
+// CHECK-NEXT:          ImplicitCastExpr 0x{{[^ ]+}} <col:{{[0-9]+}}> 'unsigned short' <IntegralCast>
+// CHECK-NEXT:            IntegerLiteral 0x{{[^ ]+}} <col:{{[0-9]+}}> 'int' 3
+// CHECK-NEXT:          ImplicitCastExpr 0x{{[^ ]+}} <col:{{[0-9]+}}> 'unsigned short' <IntegralCast>
+// CHECK-NEXT:            IntegerLiteral 0x{{[^ ]+}} <col:{{[0-9]+}}> 'int' 5
+// CHECK-NEXT:          ImplicitCastExpr 0x{{[^ ]+}} <col:{{[0-9]+}}> 'unsigned short' <IntegralCast>
+// CHECK-NEXT:            IntegerLiteral 0x{{[^ ]+}} <col:{{[0-9]+}}> 'int' 7
+// CHECK-NEXT:          ImplicitCastExpr 0x{{[^ ]+}} <col:{{[0-9]+}}> 'unsigned short' <IntegralCast>
+// CHECK-NEXT:            IntegerLiteral 0x{{[^ ]+}} <col:{{[0-9]+}}> 'int' 11
+// CHECK-NOT:          ImplicitCastExpr
+}
---
+++ test/CMFE/vec_init/vec_init_codegen_c.cpp
@@ -0,0 +1,12 @@
+// RUN: %cmc -g0 -march=SKL -emit-llvm -S -o %t.ll -- %s
+// RUN: FileCheck %s --input-file %t.ll
+
+// Check that uninitialized vector elements default to 0
+
+#include <cm/cm.h>
+
+_GENX_ void test_func() {
+  vector<unsigned short, 8> vec = {};
+// CHECK: [[VECTOR:%[^ ]+]] = alloca <8 x i16>
+// CHECK-NEXT: store <8 x i16> zeroinitializer, <8 x i16>* [[VECTOR]]
+}
