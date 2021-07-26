6dfb7356f2b78d9160e9cbe31af5f02640d6578b
mark.goncharov@intel.com <mark.goncharov@intel.com>
2021-07-26 07:14:57 +0000

    Add CM_HAS_feature macros
    Macros control versions of different instruction and platforms
    Provide new interface CM_HAS_feature - look at cm_has_instr.h

---
+++ lib/Headers/cm/include/cm/cm_has_instr.h
@@ -0,0 +1,56 @@
+/*========================== begin_copyright_notice ============================
+
+Copyright (C) 2021 Intel Corporation
+
+SPDX-License-Identifier: MIT
+
+============================= end_copyright_notice ===========================*/
+
+// This file provide CM_HAS_<Feature> flag
+// It's used for checking platform to support <Feature> 
+// 
+// USAGE:
+//   #if defined(CM_HAS_feature)
+//     w = cm_feature<...>(...);
+//   #else
+//     w = ...; //without feature
+//   #endif
+
+#ifndef _CLANG_CM_HAS_INSTR_H_
+#define _CLANG_CM_HAS_INSTR_H_
+
+#ifndef CM_HAS_CONTROL
+ 
+namespace CheckVersion {
+
+  //used for creating static_assert only on 2nd stage of
+  //substitution - when user tries to get blocked instruction
+  template <bool checking>
+  struct VersionWrapper final {
+    static constexpr bool check = checking;
+  };
+
+  template <typename T>
+  void Check() {
+    CM_STATIC_ERROR(T::check, "Not supported feature for this platform");
+  }
+}
+
+#define CM_HAS_CONTROL(checking_statement) CheckVersion::Check<CheckVersion::VersionWrapper<checking_statement>>()
+
+//-----------------------------------------------
+//-----------------------------------------------
+
+#if (CM_GENX >= 1200) 
+  #define CM_HAS_DP4A 1
+  #define CM_HAS_DP4A_CONTROL CM_HAS_CONTROL(true)
+#else
+  #define CM_HAS_DP4A_CONTROL CM_HAS_CONTROL(false)
+#endif
+
+
+#else  // CM_HAS_CONTROL
+  CM_STATIC_ERROR(0, "Redeclaration of CM_HAS_CONTROL! It's used for control version of features!");
+#endif // CM_HAS_CONTROL
+
+#endif /* _CLANG_CM_HAS_INSTR_H_ */
