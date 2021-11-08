c803b5793ed448bf1f89b4bc845c44266e3b2993
Kirill Zhavoronkov <kirill.zhavoronkov@intel.com>
2021-11-08 16:24:09 +0000

    Extend LD_LIBRARY_PATH in CMSDK setenv
    Extend LD_LIBRARY_PATH instead of overwriting

--- clang/compute_sdk/scripts/setupenv.sh
+++ clang/compute_sdk/scripts/setupenv.sh
@@ -13,7 +13,7 @@ export CSDK_DIR=$ROOT_PATH
 export CM_INCLUDE_DIR=$CSDK_DIR/usr/include
 export OPENCL_VENDOR_PATH=$CSDK_DIR/etc/OpenCL/vendors
 export PATH=$CSDK_DIR/usr/bin:$CSDK_DIR/usr/local/bin:$PATH
-export LD_LIBRARY_PATH=$CSDK_DIR/usr/lib:$CSDK_DIR/usr/lib/x86_64-linux-gnu/:$CSDK_DIR/usr/local/lib/intel-opencl:$CSDK_DIR/usr/local/lib
+export LD_LIBRARY_PATH=$CSDK_DIR/usr/lib:$CSDK_DIR/usr/lib/x86_64-linux-gnu/:$CSDK_DIR/usr/local/lib/intel-opencl:$CSDK_DIR/usr/local/lib:$LD_LIBRARY_PATH
 
 echo "Updated environment:"
 echo "CSDK_DIR=$CSDK_DIR"
