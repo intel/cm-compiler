527337b2ab4ca6bd39914c24bbf45556b0d7d5bf
Konstantin Vladimirov <konstantin.vladimirov@intel.com>
2022-03-10 16:15:03 +0000

    Fix -mCM_max_slm in clang
    We had option non-functioning, make it work again, adding LIT

---
+++ clang/test/CMFE/compiler_options/max_slm.cpp
@@ -0,0 +1,37 @@
+/*========================== begin_copyright_notice ============================
+
+Copyright (C) 2021 Intel Corporation
+
+SPDX-License-Identifier: MIT
+
+============================= end_copyright_notice ===========================*/
+
+// Tests that option -mCM_max_slm works
+
+// check that test have too much SLM
+// RUN: not %cmc -emit-llvm -S -march=pvc -fcmocl -g0 -- %s >& %t.err
+// RUN: FileCheck --check-prefix=ERRX -allow-empty %s < %t.err
+
+// check that option works:
+// RUN: %cmc -emit-llvm -S -march=pvc -mCM_max_slm=1024 -fcmocl -g0 -- %s >& %t.ok
+// RUN: FileCheck --check-prefix=GOODX -allow-empty %s < %t.ok
+
+// ERRX: use slm, but slm size is too large
+// GOODX-NOT: use slm, but slm size is too large
+
+#include <cm/cm.h>
+
+constexpr unsigned slm_size = 200 * 1024;
+
+_GENX_MAIN_
+void slm_test(SurfaceIndex ibuf [[type("buffer_t")]],
+              SurfaceIndex obuf [[type("buffer_t")]]) {
+  cm_slm_init(slm_size);
+  auto slm = cm_slm_alloc(slm_size);
+  constexpr unsigned slm_grain = 8 * 16 / sizeof(unsigned);
+  for (unsigned i = 0; i < slm_size; i += slm_grain) {
+    vector<unsigned, slm_grain> tmp = cm_load<unsigned, slm_grain>(ibuf, i);
+    cm_store_slm(slm + i, tmp);
+  }
+}
+
