a75869367a2509c5d3febf5179a7a558f9a91ba9
Semenov, Vadim <vadim.semenov@intel.com>
2025-04-09 21:29:14 +0000

    BMG device update
    .

--- clang/lib/Driver/ToolChains/Arch/GenXPlatforms.cpp
+++ clang/lib/Driver/ToolChains/Arch/GenXPlatforms.cpp
@@ -91,6 +91,8 @@ static const std::unordered_map<std::string, uint32_t> PciId {
   {"0xe20b", encodeGmdId(20, 1, 0)},
   {"0xe20c", encodeGmdId(20, 1, 0)},
   {"0xe20d", encodeGmdId(20, 1, 0)},
+  {"0xe210", encodeGmdId(20, 1, 0)},
+  {"0xe211", encodeGmdId(20, 1, 0)},
   {"0xe212", encodeGmdId(20, 1, 0)},
   {"0xe215", encodeGmdId(20, 1, 0)},
   {"0xe216", encodeGmdId(20, 1, 0)},
