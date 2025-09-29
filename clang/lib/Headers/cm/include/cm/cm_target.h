320b7aeaa0f17f11e13484fa4ef3370579f93b18
Semenov, Vadim <vadim.semenov@intel.com>
2025-09-29 21:36:58 +0000

    Add support for BMG-g31 ip version
    .

--- clang/lib/Headers/cm/include/cm/cm_target.h
+++ clang/lib/Headers/cm/include/cm/cm_target.h
@@ -146,7 +146,8 @@ static_assert(0, "CM:w:cm_target.h should not be included explicitly");
 #endif
 
 // BMG
-#if __CM_INTEL_TARGET_CORE == __CM_INTEL_TARGET(20, 1, 0)
+#if __CM_INTEL_TARGET_CORE == __CM_INTEL_TARGET(20, 1, 0) ||                   \
+    __CM_INTEL_TARGET_CORE == __CM_INTEL_TARGET(20, 2, 0)
 #define CM_GENX 1290
 #define CM_XE2_HPG
 #endif
