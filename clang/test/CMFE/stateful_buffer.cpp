146c120d4e523d5d7ef2043b799cb7e03f85f1e7
Semenov, Vadim <vadim.semenov@intel.com>
2026-04-22 16:03:28 +0000

    Add an option to suppress stateful buffer warnings
    This change introduces a new compiler option to suppress warnings
    related to stateful buffers when targeting specific hardware platforms.

--- clang/test/CMFE/stateful_buffer.cpp
+++ clang/test/CMFE/stateful_buffer.cpp
@@ -11,6 +11,7 @@ void test(SurfaceIndex buf [[type("buffer_t")]]) {
 }
 
 // RUN: %cmc -emit-llvm -march=dg2 -- %s 2>&1 | count 0
+// RUN: %cmc -emit-llvm -march=pvc -Wno-cm-buffer-not-supported -- %s 2>&1 | count 0
 
 // RUN: %cmc -emit-llvm -march=pvc -- %s 2>&1 | FileCheck --check-prefix=PVC %s
 // PVC: stateful_buffer.cpp(19{{, ?}}24): warning: stateful buffer 'buffer_t' is not supported by the level zero runtime on the given target
